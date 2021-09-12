dd4763dc8e0bca5cccc2939ac1dd419281337010
PrajayG <17574485+PrajayG@users.noreply.github.com>
2021-09-12 19:14:37 +0100

    Initial gridsome build

---
+++ boswell/src/layouts/Default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="layout">
+
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-image: url("../mobile-associates.jpg");
+  background-size: cover;
+  background-color: #cccccc;
+
+}
+
+
+
+</style>
---
+++ boswell/src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <audio id="bond" controls autoplay>
+  <source src="../bond.mp3" type="audio/mp3">
+    </audio>
+    <!-- <g-image class="background" src="~/mobile-associates.jpg"/> -->
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Boswell & Associates'
+  },
+  mounted() {
+    // console.log(document.getElementById("bond"))
+    //   document.getElementById("bond").volume = 1;
+  }
+}
+</script>
+
+<style>
+
+.background {
+  width: 100%;
+  height: 100%;
+}
+
+audio {
+  display: none;
+}
+</style>
