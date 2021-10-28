a3fb81fbc53ad1a0161cab6a0738ff1fb0a66d28
PrajayG <17574485+PrajayG@users.noreply.github.com>
2021-10-28 21:46:37 +0100

    adds eslint config, animation tweaks

--- src/components/floating-image.js
+++ src/components/floating-image.js
@@ -19,43 +19,39 @@ export default class FloatingImage extends PureComponent {
     let nw = Math.trunc(Math.floor(Math.random() * w));
     console.log([nh, nw])
     return [nh, nw];
-  };
+  }
 
   animateDiv(target) {
-    console.log(target.parentElement);
     let newq = this.makeNewPosition(target.parentElement);
     let oldq = target.getBoundingClientRect();
-
-
-    console.log(oldq ,'old q');
-    console.log(newq, 'new q');
-    let speed = this.calcSpeed([oldq.top, oldq.left], newq);
+    // let speed = this.calcSpeed([oldq.top, oldq.left], newq);
 
     target.animate([
-      { top: Math.trunc(oldq.top)+'px', left: Math.trunc(oldq.left)+'px', opacity: 0 },
-      { top: newq[0]+'px', left: newq[1]+'px', opacity: 1 }
-    ], {
-      // timing options
-      duration: 10000,
-      iterations: 3
-    })
+    { top: Math.trunc(oldq.top)+'px', left: Math.trunc(oldq.left)+'px', opacity: 0 },
+    { top: newq[0]+'px', left: newq[1]+'px', opacity: 1 }
+  ], {
+    // timing options
+    duration: 1000,
+    iterations: 3
+})
+
 
   }
 
-  calcSpeed(prev, next) {
+  // calcSpeed(prev, next) {
 
-    let x = Math.abs(prev[1] - next[1]);
-    let y = Math.abs(prev[0] - next[0]);
+  //   let x = Math.abs(prev[1] - next[1]);
+  //   let y = Math.abs(prev[0] - next[0]);
 
-    let greatest = x > y ? x : y;
+  //   let greatest = x > y ? x : y;
 
-    let speedModifier = 1000;
+  //   let speedModifier = 1000;
 
-    let speed = Math.ceil(greatest / speedModifier);
+  //   let speed = Math.ceil(greatest / speedModifier);
 
-    return speed;
+  //   return speed;
 
-  }
+  // }
 
   render() {
 
@@ -70,6 +66,7 @@ export default class FloatingImage extends PureComponent {
     let target = document.querySelector('.floating-image');
     console.log(target)
     this.animateDiv(target);
+
 
   }
 
