5035da44809ea6999ee7e76b9fff5ada37ab234c
PrajayG <17574485+PrajayG@users.noreply.github.com>
2021-10-28 20:22:41 +0100

    fixes animation component

--- src/components/floating-image.js
+++ src/components/floating-image.js
@@ -1,61 +1,57 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
-import { StaticImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image"
 
 export default class FloatingImage extends PureComponent {
   static propTypes = {
     image: PropTypes.string
   };
 
-  // http://jsfiddle.net/j2PAb/ <-- For animating, just rewrite this in native js
+  // http://jsfiddle.net/j2PAb/ <-- rewrite this in native js
 
 
   makeNewPosition(container) {
 
-    let h = container.offsetWidth - 50;
-    let w = container.offsetHeightq - 100;
+    let h = container.offsetWidth;
+    let w = container.offsetHeight;
 
-    let nh = Math.floor(Math.random() * h);
-    let nw = Math.floor(Math.random() * w);
-
+    let nh = Math.trunc(Math.floor(Math.random() * h));
+    let nw = Math.trunc(Math.floor(Math.random() * w));
+    console.log([nh, nw])
     return [nh, nw];
   };
 
   animateDiv(target) {
-    console.log(target.parentElement)
-    var newq = this.makeNewPosition(target.parentElement);
-    var oldq = target.offset;
+    console.log(target.parentElement);
+    let newq = this.makeNewPosition(target.parentElement);
+    let oldq = target.getBoundingClientRect();
 
-    var speed = this.calcSpeed([oldq.top, oldq.left], newq);
-    console.log(speed);
+
+    console.log(oldq ,'old q');
+    console.log(newq, 'new q');
+    let speed = this.calcSpeed([oldq.top, oldq.left], newq);
+
     target.animate([
-      { top: oldq.top, left: oldq.left },
-      { top: newq[0], left: newq[1] }
+      { top: Math.trunc(oldq.top)+'px', left: Math.trunc(oldq.left)+'px', opacity: 0 },
+      { top: newq[0]+'px', left: newq[1]+'px', opacity: 1 }
     ], {
-      //timing options
-      duration: speed,
-      iterations: Infinity
+      // timing options
+      duration: 10000,
+      iterations: 3
     })
 
-    // $target.animate({
-    //     top: newq[0],
-    //     left: newq[1]
-    // }, speed, function() {
-    //     animateDiv($target);
-    // });
-
-  };
+  }
 
   calcSpeed(prev, next) {
 
-    var x = Math.abs(prev[1] - next[1]);
-    var y = Math.abs(prev[0] - next[0]);
+    let x = Math.abs(prev[1] - next[1]);
+    let y = Math.abs(prev[0] - next[0]);
 
-    var greatest = x > y ? x : y;
+    let greatest = x > y ? x : y;
 
-    var speedModifier = 0.4;
+    let speedModifier = 1000;
 
-    var speed = Math.ceil(greatest / speedModifier);
+    let speed = Math.ceil(greatest / speedModifier);
 
     return speed;
 
@@ -65,9 +61,7 @@ export default class FloatingImage extends PureComponent {
 
     return (
       <div className="floating-image" >
-        {this.props.image}
         <StaticImage src="../images/1.png" alt="" />
-        Here is some random text
       </div>
     )
   }
@@ -76,6 +70,7 @@ export default class FloatingImage extends PureComponent {
     let target = document.querySelector('.floating-image');
     console.log(target)
     this.animateDiv(target);
+
   }
 
 }
