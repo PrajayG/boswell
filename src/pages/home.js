90d2ea0d883e0e2e630e1b6361bd96cdee4b7f65
PrajayG <17574485+PrajayG@users.noreply.github.com>
2021-10-31 22:02:35 +0000

    adds cyberpunk css styles

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const Home = () => (
+      <main>
+      <title>Boswell & Associates</title>
+      <div className="container">
+        Welcome, thank you for allowing access. You may proceed.
+      </div>
+      </main>
+)
+
+export default Home;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "../styles/main.scss";
 import FloatingImage from "../components/floating-image";
-
+import { Link } from "gatsby";
 // data
 
 // markup
@@ -11,8 +11,23 @@ const IndexPage = () => {
       <title>Boswell & Associates</title>
 
         <div className="container">
-          <h1>Boswell & Associates</h1>
+          <h1 className="cyberpunk glitched">Boswell & Associates</h1>
+          <section className="cyberpunk black">
+            <p>In order to access Boswell & Associates, please allow access to device microphone, camera and memory.</p>
 
+            <Link to="/home">
+              <button className="cyberpunk2077 green">
+                Yes
+              </button>
+            </Link>
+
+
+            <Link to="/home">
+              <button className="cyberpunk2077 red">
+                No
+              </button>
+            </Link>
+          </section>
           <FloatingImage image="This works"></FloatingImage>
         </div>
     </main>
